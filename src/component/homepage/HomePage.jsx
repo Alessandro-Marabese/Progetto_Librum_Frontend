7aae06a341dc4abab55b925eeb55a74e25959a4c
Alessandro-Marabese <alessandro.marabese93@gmail.com>
2025-06-10 17:57:33 +0200

    homepage css

--- src/component/homepage/HomePage.jsx
+++ src/component/homepage/HomePage.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getCurrentUser, getFriendsByUser, getReviewsByUserHomepage, getUserBookByUser, getUserById } from "../../redux/actions";
 import { Col, Container, Row, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./HomePage.css";
 
 function HomePage() {
   const dispatch = useDispatch();
@@ -49,21 +50,21 @@ function HomePage() {
   }, [reviews, usersReviewsById, dispatch]);
 
   return (
-    <Container>
-      <Row>
-        <Col className="col-4">
+    <Container className="homepage-container">
+      <Row className="homepage-row">
+        <Col className="homepage-sidebar col-4">
           {currentlyReading.length > 0 && (
-            <Row className="border-bottom mt-3 pb-3">
-              <h3>Currently Reading</h3>
+            <Row className="book-section border-bottom mt-3 pb-3">
+              <h3 className="section-title">Currently Reading</h3>
               {currentlyReading.slice(0, 1).map((item) => (
                 <Col key={item.libro.id}>
                   <div className="d-flex">
-                    <div>
+                    <div className="book-item">
                       <Link to={`/libro/${encodeURIComponent(item.libro.id)}`}>
                         <img src={item.libro.coverUrl} alt={item.libro.titolo} className="img-fluid" />
                       </Link>
                     </div>
-                    <div className="ms-2">
+                    <div className="ms-2 book-info">
                       <Link to={`/libro/${encodeURIComponent(item.libro.id)}`}>
                         <h5>{item.libro.titolo}</h5>
                       </Link>
@@ -72,7 +73,7 @@ function HomePage() {
                       </Link>
                     </div>
                   </div>
-                  <div>
+                  <div className="book-actions">
                     <Link to={`/mybooks`}>View All Books</Link>
                     <Link to={`/search`} className="ms-2">
                       Add a Book
@@ -83,18 +84,18 @@ function HomePage() {
             </Row>
           )}
           {wantToRead.length > 0 && (
-            <Row className="border-bottom mt-3 pb-3">
-              <h3>Want To Read</h3>
+            <Row className="book-section border-bottom mt-3 pb-3">
+              <h3 className="section-title">Want To Read</h3>
               {wantToRead.slice(0, 3).map((item) => (
                 <Col key={item.libro.id} className="d-flex">
-                  <div className="col-4">
+                  <div className="book-item col-4">
                     <Link to={`/libro/${encodeURIComponent(item.libro.id)}`}>
                       <img src={item.libro.coverUrl} alt={item.libro.titolo} className="img-fluid" />
                     </Link>
                   </div>
                 </Col>
               ))}
-              <div>
+              <div className="book-actions">
                 <Link to={`/mybooks`}>View All Books</Link>
                 <Link to={`/search`} className="ms-2">
                   Add a Book
@@ -103,9 +104,9 @@ function HomePage() {
             </Row>
           )}
           <Row>
-            <Col className=" mt-3">
-              <h3>My Bookshelves</h3>
-              <ul className="mybooks-first-list">
+            <Col className="book-section mt-3">
+              <h3 className="section-title">My Bookshelves</h3>
+              <ul className="bookshelf-list">
                 <li>
                   <Link to={`/mybooks`}>Want to Read</Link> ({wantToRead.length}){" "}
                 </li>
@@ -122,16 +123,16 @@ function HomePage() {
             </Col>
           </Row>
         </Col>
-        <Col className="col-8">
-          <h3>Updates from your friends</h3>
+        <Col className="homepage-main col-8">
+          <h3 className="section-title">Updates from your friends</h3>
           {reviews.length > 0 &&
             reviews
               .sort((a, b) => new Date(b.dataCreazione) - new Date(a.dataCreazione))
               .map((review) => {
                 const user = usersReviewsById[review.utenteId];
                 return (
-                  <Row key={review.id} className="border-bottom mt-3">
-                    <Col className="col-3">
+                  <Row key={review.id} className="review-item border-bottom mt-3">
+                    <Col className="review-user col-3">
                       {user ? (
                         <>
                           <Link to={`/profile/${user.id}`}>
@@ -147,9 +148,9 @@ function HomePage() {
                         <Spinner animation="border" />
                       )}
                     </Col>
-                    <Col className="col-4">
-                      <p>{review.commento}</p>
-                      <div>
+                    <Col className="review-content col-4">
+                      <p className="review-comment">{review.commento}</p>
+                      <div className="review-rating">
                         {[1, 2, 3, 4, 5].map((num) => (
                           <ion-icon
                             key={num}
@@ -163,15 +164,15 @@ function HomePage() {
                           ></ion-icon>
                         ))}
                       </div>
-                      <p>{review.dataCreazione}</p>
+                      <p className="review-date">{review.dataCreazione}</p>
                     </Col>
-                    <Col className="col-5 d-flex">
-                      <div>
+                    <Col className="col-5 d-flex align-items-start">
+                      <div className="review-book">
                         <Link to={`/libro/${encodeURIComponent(review.libro.libroId)}`}>
                           <img src={review.libro.coverUrl} alt={review.libro.titolo} className="img-fluid" style={{ width: "auto", height: "100px" }} />
                         </Link>
                       </div>
-                      <div className="ms-2">
+                      <div className="book-details ms-2">
                         <Link to={`/libro/${encodeURIComponent(review.libro.libroId)}`}>
                           <p>{review.libro.titolo}</p>
                         </Link>
